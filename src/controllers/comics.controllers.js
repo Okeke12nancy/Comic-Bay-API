b5a649d8026c500eb496562f714456f9da91088c
Nancy Okeke <ogadimmanancy12@gmail.com>
2023-04-14 21:06:02 -0700

    commented some files

--- src/controllers/comics.controllers.js
+++ src/controllers/comics.controllers.js
@@ -1,32 +1,32 @@
-import BaseUrlClassService from "../services/comic.service";
+// import BaseUrlClassService from "../services/comic.service";
 
-export class BaseUrlClassController {
-  async getAll(req, res) {
-    const urlType = req.params.urlType;
-    const query = req.body.query;
+// export class BaseUrlClassController {
+//   async getAll(req, res) {
+//     const urlType = req.params.urlType;
+//     const query = req.body.query;
 
-    const data = await newUrl.urlMapperGetAll(urlType, query);
+//     const data = await newUrl.urlMapperGetAll(urlType, query);
 
-    if (data.length === 0) {
-      res.status(404).json({ success: false, message: `data not found`, data });
-    } else {
-      res.json({ success: true, message: `data fetched successfuly`, data });
-    }
-  }
-  async getById(req, res) {
-    const urlType = req.params.urlType;
-    const id = req.params.id;
-    const limit = req.body.limit;
-    const data = await newUrl.urlMapperGetById(urlType, limit, id);
+//     if (data.length === 0) {
+//       res.status(404).json({ success: false, message: `data not found`, data });
+//     } else {
+//       res.json({ success: true, message: `data fetched successfuly`, data });
+//     }
+//   }
+//   async getById(req, res) {
+//     const urlType = req.params.urlType;
+//     const id = req.params.id;
+//     const limit = req.body.limit;
+//     const data = await newUrl.urlMapperGetById(urlType, limit, id);
 
-    if (data.length === 0) {
-      res.status(404).json({
-        success: false,
-        message: `data not found with id: [${id}]`,
-        data,
-      });
-    } else {
-      res.json({ success: true, message: `data fetched successfuly`, data });
-    }
-  }
-}
+//     if (data.length === 0) {
+//       res.status(404).json({
+//         success: false,
+//         message: `data not found with id: [${id}]`,
+//         data,
+//       });
+//     } else {
+//       res.json({ success: true, message: `data fetched successfuly`, data });
+//     }
+//   }
+// }
--- src/services/comic.service.js
+++ src/services/comic.service.js
@@ -1,35 +1,15 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const axios = require("axios");
-app.use(bodyParser.json());
+//   async urlMapperGetById(urlType, limit, id) {
+//     return await axios
+//       .get(`${baseUrl}${urlType}/${id}?api_key=${API_KEY}&format=json`)
+//       .then((response) => {
+//         const singleData = response.data.results;
+//         return singleData;
+//       })
+//       .catch((error) => {
+//         if (error.code === "ERR_BAD_REQUEST") return [];
+//         return error;
+//       });
+//   }
+// }
 
-class BaseUrlClassService {
-  async urlMapperGetAll(urlType, query) {
-    return await axios
-      .get(`${baseUrl}${urlType}?api_key=${API_KEY}&format=json&query=${query}`)
-      .then((response) => {
-        const data = response.data.results;
-        return data;
-      })
-      .catch((error) => {
-        if (error.code === "ERR_BAD_REQUEST") return [];
-        return error;
-      });
-  }
-
-  async urlMapperGetById(urlType, limit, id) {
-    return await axios
-      .get(`${baseUrl}${urlType}/${id}?api_key=${API_KEY}&format=json`)
-      .then((response) => {
-        const singleData = response.data.results;
-        return singleData;
-      })
-      .catch((error) => {
-        if (error.code === "ERR_BAD_REQUEST") return [];
-        return error;
-      });
-  }
-}
-
-export default new BaseUrlClassService();
+// export default new BaseUrlClassService();
